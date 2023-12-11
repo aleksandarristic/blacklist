e6c57b152ddd67a3454c138bf9127968a302cf5d
Aleksandar Ristic <aleksandar1983@gmail.com>
2023-12-11 15:03:03 +0100

    Adding build tool

--- README.md
+++ README.md
@@ -8,3 +8,40 @@ A collection of various blacklists of hosts for use in pihole or similar softwar
 ### Authors of original lists
 * [@milos_rs_](https://twitter.com/milos_rs_ "@milos_rs_ on X")
 * [maxxa](https://bezbedanbalkan.net/user-5.html "maxxa on Bezbedanbalkant.net")
+
+
+--
+
+
+### Usage examples for `build_list.py`
+
+Tool usage:
+
+```
+usage: build_list.py [-h] [-s SECTION] [-f FILENAME] [-t TARGET] [--run] [--debug]
+
+options:
+  -h, --help            show this help message and exit
+  -s SECTION, --section SECTION
+                        Section name (eg: "Scam" or "typosquatting").
+  -f FILENAME, --filename FILENAME
+                        File with "raw" data. See raw.md for supported formats and substitutions.
+  -t TARGET, --target TARGET
+                        Target filename. If exists, it will be updated with the new content.
+  --run                 Run the script. Otherwise just quit.
+  --debug               Debug mode. Writes a lot.
+```
+
+The `build_list.py` will parse any textual file with domains in a line-by-line format and into the blacklist compatible output file. The input file needs to meet the following criteria:
+
+* Each line is a new domain.
+* Each domain is the first word in the line.
+
+The tool will apply substitutions from `subs.json` to each line it reads from the input file (eg: it will replace `[.]` with `.` - you can add any subs you like). It will not overwrite old content - it's only ever going to add new hosts in the apropriate section. The resulting list of hosts will have unique hosts in sorted order. The tool will also write section headers and a few comment lines underneath. The idea is to run the tool for each new raw source file to populate different sections of the resulting output file. The blacklist from this repo has been built using this tool, so you can see [scam_hosts_srb.txt](/lists/scam_hosts_srb.txt) for example output.
+
+#### Usage examples:
+
+The following example will read `scam.txt` for new hosts, open the `out.txt`, find the existing section named `Scam` and add new hosts.
+```
+./build_list.py -f scam.txt -s Scam -t out.txt --run
+```
---
+++ scripts/build_list.py
@@ -0,0 +1,180 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import logging
+import argparse
+import json
+
+
+SUBS = 'subs.json'
+LOG_FILE = "build_list.log"
+
+
+log = logging.getLogger(__file__)
+
+
+def configure_logging(debug=False):
+    for handler in logging.root.handlers[:]:
+        logging.root.removeHandler(handler)
+
+    file_handler = logging.FileHandler(filename=LOG_FILE)
+    handlers = [file_handler, logging.StreamHandler(sys.stdout)]
+
+    logging.basicConfig(
+        level=logging.DEBUG if debug else logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(message)s",
+        datefmt="%d-%b-%y %H:%M:%S",
+        handlers=handlers,
+    )
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-s', '--section', action='store', dest='section', help='Section name (eg: "Scam" or "typosquatting").')
+    parser.add_argument('-f', '--filename', action='store', dest='filename', help='File with "raw" data. See raw.md for supported formats and substitutions.')
+    parser.add_argument('-t', '--target', action='store', dest='target', help='Target filename. If exists, it will be updated with the new content.')
+    
+    parser.add_argument('--run', action='store_true', dest='run', default=False, help='Run the script. Otherwise just quit.')
+    parser.add_argument('--debug', action='store_true', dest='debug', default=False, help='Debug mode. Writes a lot.')
+
+    args = parser.parse_args()
+    
+    configure_logging(args.debug)
+    log.debug(args)
+    return args
+
+
+def load_subs(filename):
+    with open(filename, 'r') as f:
+        return json.loads(f.read())
+    
+
+def load_new_data(filename):
+    try:
+        with open(filename, 'r') as f:
+            data = []
+            lines = f.readlines()
+            for l in lines:
+                data.append(parse_line(l))
+            return data
+    except Exception as e:
+        log.error(f'Could not load "{filename}": {e}')
+        return []
+
+
+def parse_line(line):
+    # assume ' '-separated domain is the first word in the line
+    if ' ' in line:
+        line = line.split(' ')[0].lstrip()
+
+    # do substitutions
+    for orig, sub in load_subs(SUBS).items():
+        line = line.replace(orig, sub)
+
+    return line
+
+
+def parse_target(target_file):
+    """
+    Parses target file for existing sections and their corresponding entries
+    """
+    result = {}
+    current_section_name = None
+    with open(target_file, 'r') as f:
+        lines = f.readlines()
+
+    for l in lines:
+        l = l.strip()
+        if l.startswith('###') and l.endswith('domains start'):
+            try:
+                current_section_name = l.split(' ')[1]
+                result[current_section_name] = {'items': [], 'comments': []}
+                continue
+            except Exception as e:
+                log.error(f'Could not identify section start: {e}')
+                continue
+
+        if l.startswith('###') and l.endswith('domains end'):
+            current_section_name = None
+            continue
+
+        if l.startswith('#') and current_section_name:
+            result[current_section_name]['comments'].append(l)
+            continue
+
+        if l and current_section_name:
+            result[current_section_name]['items'].append(l)
+    
+    log.debug(result)
+    return result
+
+
+def write_data(data, target):
+    with open(target, 'w') as f:
+        for section, section_data in data.items():
+            log.debug(f'Writing section "{section}".')
+            f.write(f'\n### {section} domains start\n')
+
+            for comment_line in section_data['comments']:
+                if not comment_line.endswith('\n'): comment_line += '\n'
+                f.write(comment_line)
+            f.write('\n')
+
+            for line in section_data['items']:
+                if not line.endswith('\n'): line += '\n'
+                f.write(line)
+
+            f.write('\n')
+            f.write(f'### {section} domains end\n')
+            log.debug('Done')
+        log.debug('All done')
+
+
+def main():
+    args = parse_args()
+    if not args.run or not (args.section and args.filename and args.target):
+        log.info('Nothing to do, bye!')
+        quit(0)
+
+    if args.target and not os.path.exists(args.target):
+        log.info(f'Target file "{args.target}" does not exist and will be created.')
+        with open(args.target, 'w') as f:
+            print('', file=f)
+        
+    data = parse_target(args.target)
+
+    if args.section in data.keys():
+        log.info(f'Section "{args.section}" found in "{args.target}" and will be updated.')
+        initial_record_count = len(data[args.section]['items'])
+        log.debug(f'Initial count of section "{args.section}" is: {initial_record_count}.')
+
+    else:
+        log.info(f'Section "{args.section}" was not found in "{args.target}". New section will be created.')
+        data[args.section] = {
+            'items': [],
+            'comments': []
+        }
+        initial_record_count = 0
+
+    new_data = load_new_data(args.filename)
+    log.debug(f'Loaded {len(new_data)} new records.')
+    
+    data[args.section]['items'].extend(new_data)
+    data[args.section]['items'] = sorted(list(set(data[args.section]['items']))) # ugly hack to have a sorted list of unique lines
+    updated_count = len(data[args.section]['items'])
+
+    log.debug(f'Updated record count is: {updated_count}')
+    log.debug(f'Updated data: {data}')
+    log.info(f'Added {updated_count - initial_record_count} new unique records to the section "{args.section}" in file "{args.target}".')
+    log.info(f'Writing data to "{args.target}"...')
+    write_data(data, args.target)
+    
+    log.info('All done!')
+
+
+
+if __name__ == '__main__':
+    main()
