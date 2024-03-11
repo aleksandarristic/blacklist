d5678283aea7131e488dbee5e52986e4d3397309
Aleksandar Ristic <aleksandar1983@gmail.com>
2024-03-11 16:18:15 +0100

    Typing - second pass; docstrings.

--- scripts/build_list.py
+++ scripts/build_list.py
@@ -7,13 +7,22 @@ import logging
 import os
 import sys
 
+from typing import Dict, List, AnyStr
+
 SUBS = 'subs.json'
-LOG_FILE = "build_list.log"
+LOG_FILE = 'build_list.log'
 
 log = logging.getLogger(__file__)
 
 
 def configure_logging(debug: bool = False) -> None:
+    """
+    Configure logging.
+    :param debug: If set to True, use debug logging
+    :type debug: bool
+    :return:
+    :rtype:
+    """
     for handler in logging.root.handlers[:]:
         logging.root.removeHandler(handler)
 
@@ -21,11 +30,16 @@ def configure_logging(debug: bool = False) -> None:
     handlers = [file_handler, logging.StreamHandler(sys.stdout)]
 
     logging.basicConfig(level=logging.DEBUG if debug else logging.INFO,
-                        format="%(asctime)s - %(levelname)s - %(message)s", datefmt="%d-%b-%y %H:%M:%S",
+                        format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S',
                         handlers=handlers, )
 
 
 def parse_args() -> argparse.Namespace:
+    """
+    Wrapper for parsing arguments using argparse
+    :return: Arguments parsed from the sys.argv
+    :rtype: argparse.Namespace
+    """
     parser = argparse.ArgumentParser()
 
     parser.add_argument('-s', '--section', action='store', dest='section',
@@ -46,12 +60,26 @@ def parse_args() -> argparse.Namespace:
     return args
 
 
-def load_subs(filename: str) -> dict:
+def load_subs(filename: AnyStr) -> Dict[AnyStr:AnyStr]:
+    """
+    Load substitution map from file.
+    :param filename: File with substitutions.
+    :type filename: AnyStr
+    :return: Map of substitutions
+    :rtype: Dict[AnyStr:AnyStr]
+    """
     with open(filename, 'r') as f:
         return json.loads(f.read())
 
 
-def load_new_data(filename: str) -> list:
+def load_new_data(filename: AnyStr) -> List[AnyStr]:
+    """
+    Loads new data from the specified filename.
+    :param filename: File to load data from
+    :type filename: AnyStr
+    :return: List of parsed lines.
+    :rtype: List[AnyStr]
+    """
     try:
         with open(filename, 'r') as f:
             data = []
@@ -64,7 +92,16 @@ def load_new_data(filename: str) -> list:
         return []
 
 
-def parse_line(line: str) -> str:
+def parse_line(line: AnyStr) -> AnyStr:
+    """
+    Parses a line to apply the following transformations:
+    - Take the part of the line just up until the first space character (assumes space-separated lines format)
+    - Apply substitiutions as defined in the substitutions file.
+    :param line: A line to parse
+    :type line: AnyStr
+    :return: Returns a parsed line
+    :rtype: AnyStr
+    """
     # assume ' '-separated domain is the first word in the line
     if ' ' in line:
         line = line.split(' ')[0].lstrip()
@@ -76,9 +113,16 @@ def parse_line(line: str) -> str:
     return line
 
 
-def parse_target(target_file: str) -> dict:
+def parse_target(target_file: AnyStr) -> Dict[AnyStr:Dict[AnyStr:List[AnyStr]]]:
     """
-    Parses target file for existing sections and their corresponding entries
+    Parses the target file for existing sections and their corresponding entries.
+    The returned data will have a section name as the first level key and a dictionary
+    with 'items' and 'comments' keys as value. Both 'items' and 'comments' values are
+    lists of AnyStr.
+    :param target_file: Filename of the file to read from
+    :type target_file: AnyStr
+    :return: A map with section names as the key and section data as value
+    :rtype: Dict[AnyStr:Dict[AnyStr:List[AnyStr]]]
     """
     result = {}
     current_section_name = None
@@ -111,7 +155,16 @@ def parse_target(target_file: str) -> dict:
     return result
 
 
-def write_data(data: dict, target: str) -> None:
+def write_data(data: Dict[AnyStr:Dict[AnyStr:List[AnyStr]]], target: AnyStr) -> None:
+    """
+    Write the data structure to the blacklist file.
+    :param data: Data to write.
+    :type data: Dict[AnyStr:Dict[AnyStr:List[AnyStr]]]
+    :param target:
+    :type target: AnyStr
+    :return:
+    :rtype: None
+    """
     with open(target, 'w') as f:
         for section, section_data in data.items():
             log.debug(f'Writing section "{section}".')
